3877fb102b8362c48b8b50406df912b8e71e30de
Reem35 <43762012+Reem35@users.noreply.github.com>
2018-10-27 19:50:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`1play | Stars Shom`,"http://twitch.tv/akrame35")
+client.user.setGame(`1play | Stars Shop`,"http://twitch.tv/akrame35")
 client.user.setStatus("dnd")
 });
 
